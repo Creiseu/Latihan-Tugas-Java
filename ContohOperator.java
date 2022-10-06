ed07151083e9e1978a140f01af4ecc542982c777
Ud <93574282+Creiseu@users.noreply.github.com>
2022-10-06 09:43:23 +0700

    Add files via upload

---
+++ ContohOperator.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.contohoperator;
+
+/**
+ *
+ * @author User
+ */
+public class ContohOperator {
+
+    public static void main(String[] args){
+        
+        int a = 10, b = 20, c, d = 5, e, f;
+        float x = (float) 3.14, y = (float) 22.7;
+        float hasil = x + y;
+        double g;
+        g= a / b;
+        c = a % b;
+        
+        e = d--;//5
+        f = --d;//5-1-1
+        //boolean hasil = a > b;
+        System.out.println("Belajar Java");
+        System.out.println("Hasil dari C = "+c);
+        System.out.println("Hasil dari g = "+g);
+        System.out.println("Hasil dari E : "+e);
+        System.out.println("Hasil dari f : "+f);
+        //System.out.println("Hasil boolean = "+hasil);
+        System.out.println("Hasil float = "+hasil);
+    }
+}
